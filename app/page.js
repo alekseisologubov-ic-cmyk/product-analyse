05aff1c01d221d3cf92a3f776d49c37373191104
alekseisologubov-ic-cmyk <aleksei.sologubov-ic@virginvoyages.com>
2026-05-11 15:47:47 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -456,11 +456,12 @@ const [emailMessage, setEmailMessage] = useState("");
 const [otpLoading, setOtpLoading] = useState(false);
   const [message, setMessage] = useState("");
   const [viewMode, setViewMode] = useState("all");
+  
 const [showProductMissingReport, setShowProductMissingReport] = useState(false);
 const [productMissingReportRows, setProductMissingReportRows] = useState([]);
 const [productMissingReportLoading, setProductMissingReportLoading] = useState(false);
 const [productMissingReportMessage, setProductMissingReportMessage] = useState("");
-const [nextOrderRows, setNextOrderRows] = useState([]);
+
   const [nextOrderSourceRows, setNextOrderSourceRows] = useState([]);
   const [nextOrderMeta, setNextOrderMeta] = useState({});
   const [nextOrderFileName, setNextOrderFileName] = useState("");
