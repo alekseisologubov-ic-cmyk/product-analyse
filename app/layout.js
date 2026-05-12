0040b3a254ba70794aa54cc510c80298388eb032
alekseisologubov-ic-cmyk <aleksei.sologubov-ic@virginvoyages.com>
2026-05-12 15:55:54 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,17 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 export const metadata = {
-  title: "Dashboard",
+  title: "Virgin Voyages Dashboard",
+  description: "Virgin Voyages Product, Equipment, Inventory and Schedule Dashboard",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html>
-      <body>{children}</body>
+    <html lang="en">
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
