2995e4ce377fe3d68b1903e9e2a32753eecb5ec4
alekseisologubov-ic-cmyk <aleksei.sologubov-ic@virginvoyages.com>
2026-05-16 07:14:45 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -51,10 +51,9 @@ const isVirginVoyagesEmail = (value) => {
 };
 
 const USER_EMAIL_STORAGE_KEY = "vv_app_user_email";
+
 const ADMIN_EMAILS = new Set([
-  const ADMIN_EMAILS = new Set([
   "aleksei.sologubov-ic@virginvoyages.com",
-]);
   // add more admin emails here
 ]);
 
