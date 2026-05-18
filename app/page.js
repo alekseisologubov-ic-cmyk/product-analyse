a9e97d4f351a5e230eaebeb16aab8fde2a7b48b7
alekseisologubov-ic-cmyk <aleksei.sologubov-ic@virginvoyages.com>
2026-05-18 14:19:51 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7666,7 +7666,7 @@ isAdmin={isAdmin}
                             if (link) link.style.display = "block";
                           }}
                         />
-                        <a href={item.image} target="_blank" rel="noreferrer" style={styles.imageLink}>Open Picture</a>
+                        <a href={displayImage} target="_blank" rel="noreferrer" style={styles.imageLink}>Open Picture</a>
                       </div>
                     ) : (
                       <div style={styles.equipmentNoImage}>No image</div>
