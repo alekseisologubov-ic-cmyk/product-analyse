0f617938f5d379558aca647ffbf55d2be2caf676
alekseisologubov-ic-cmyk <aleksei.sologubov-ic@virginvoyages.com>
2026-05-20 07:30:53 +0100

    Update TrainingModule.jsx

--- app/components/training/TrainingModule.jsx
+++ app/components/training/TrainingModule.jsx
@@ -2,6 +2,7 @@
 
 import React, { useEffect, useMemo, useState } from "react";
 import * as XLSX from "xlsx";
+import { useAppContext } from "../../context/AppContext";
 
 const cleanText = (value) =>
   String(value || "")
